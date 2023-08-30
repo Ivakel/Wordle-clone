a926471939ab1ed3d7d2ec9fcac7cc4214523448
Ivakel <ivakeledlabom88@gmail.com>
2023-08-31 00:14:46 +0200

    input logic done

--- src/main.js
+++ src/main.js
@@ -10,13 +10,58 @@ const word = "needs";
 const wordSet = new Set(word);
 
 let row = 0;
-let col = 0;
+let col = -1;
 
-const validInput = new Set(
-  "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM"
-);
-const currWord = "";
-let allWords = new Set(["needs"]);
+const validInput = new Set("qwertyuioplkjhgfdsazxcvbnm");
+let currWord = "";
+
+let colFull = false; //checks if i can still insert a letter
+
+let allWords = new Set([
+  "needs",
+  "which",
+  "there",
+  "their",
+  "about",
+  "would",
+  "these",
+  "other",
+  "words",
+  "could",
+  "write",
+  "first",
+  "water",
+  "after",
+  "where",
+  "right",
+  "think",
+  "three",
+  "years",
+  "place",
+  "sound",
+  "great",
+  "again",
+  "still",
+  "every",
+  "small",
+  "found",
+  "those",
+  "never",
+  "under",
+  "might",
+  "while",
+  "house",
+  "world",
+  "below",
+  "asked",
+  "going",
+  "large",
+  "until",
+  "along",
+  "shall",
+  "being",
+  "nerds",
+]);
 
 function showWord() {
   const wordBox = document.getElementById("word");
@@ -33,11 +78,12 @@ function lost() {
 }
 
 function enterPress() {
-  if (col === 4) {
+  if (currWord.length === 5) {
     // all the boxes are filled
+
     if (allWords.has(currWord)) {
       //the word is a valid existing word
-      row++;
+
       for (let i = 0; i < 5; i++) {
         const letterBox = document.getElementById(`l${row + 1}${i + 1}`);
         if (currWord[col] === word[col]) {
@@ -48,7 +94,7 @@ function enterPress() {
             //correct letter, incorrect pos
             letterBox.classList.add("incorrect-pos");
           } else {
-            //correct letter
+            //incorrect letter
             letterBox.classList.add("incorrect-letter");
           }
         }
@@ -56,11 +102,14 @@ function enterPress() {
       if (currWord === word) {
         //the game is over
         gameOver();
+        return;
       }
-      if (row === 5) {
+      if (row === 6) {
         //you've lost the game
         lost();
       }
+      currWord = "";
+      row++;
     } else {
       return;
     }
@@ -68,23 +117,34 @@ function enterPress() {
   return;
 }
 
+function reduceWord() {
+  let w = "";
+  for (let i = 0; i < currWord.length - 1; i++) {
+    w += currWord[i];
+  }
+  currWord = w;
+  col--;
+}
+function increaseWord(letter) {
+  currWord += letter;
+
+  col++;
+  game[row][col] = letter;
+}
+
 function backspace() {
-  if (col === 0) {
-    game[row][col] = "";
-    const span = document.getElementById(`l${row + 1}${col + 1}`);
-    span.innerHTML = "";
+  if (col < 0) {
     return;
   }
   game[row][col] = "";
   const span = document.getElementById(`l${row + 1}${col + 1}`);
   span.innerHTML = "";
-  col--;
+  reduceWord();
 }
 
 //for the device keyboard
 document.body.addEventListener("keyup", (ev) => {
   const letter = ev.key.toLowerCase();
-  console.log("column", col);
 
   if (letter === "backspace") {
     backspace();
@@ -101,26 +161,21 @@ document.body.addEventListener("keyup", (ev) => {
     return;
   }
   //the row is full
-  if (col === 4) {
-    const letterBox = document.getElementById(`l${row + 1}${col + 1}`);
-    if (letterBox.innerHTML === "") {
-      letterBox.innerHTML = letter.toUpperCase();
-    }
-    return;
-  }
-
-  game[row][col] = letter;
-  const letterBox = document.getElementById(`l${row + 1}${col + 1}`);
-  console.log(`l${row + 1}${col + 1}`, letterBox);
-  letterBox.innerHTML = letter.toUpperCase();
-  col++;
+  insertLetter(letter);
 });
 
+function insertLetter(letter) {
+  if (col === 4) {
+    return;
+  }
+  increaseWord(letter);
+  game[row][col] = letter;
+  let letterBox = document.getElementById(`l${row + 1}${col + 1}`);
+  letterBox.innerText = letter.toUpperCase();
+}
 //for the app keyboard
 function btnClicked(el) {
   const letter = el.innerText.toLowerCase();
-  console.log(letter);
-  return;
 
   //the backspace is pressed
   if (letter === "") {
@@ -136,9 +191,5 @@ function btnClicked(el) {
   if (col >= 5) {
     return;
   }
-  currWord += letter;
-  game[row][col] = letter;
-  const letterBox = document.getElementById(`l${row + 1}${col + 1}`);
-  letterBox.innerText = letter.toUpperCase();
-  col++;
+  insertLetter(letter);
 }
